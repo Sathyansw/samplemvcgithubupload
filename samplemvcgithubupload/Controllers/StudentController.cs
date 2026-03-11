6b522343755bb3a3c658d85a300ce64aff64caa2
Sathyansw <SathyanSW.IT@gmail.com>
2026-03-11 18:27:50 +0530

    student controller added

---
+++ samplemvcgithubupload/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace samplemvcgithubupload.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
